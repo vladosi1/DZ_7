857a0be6d33245c31a5c09f30723482aecac3e3b
Vlad Vobiachenko <109553178+vladosi1@users.noreply.github.com>
2023-02-23 00:12:33 +0200

    -f commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let x = prompt("Сергей, напишите первое число)");
+let y = prompt("Сергей, напишите второе число)");
+let z = prompt("Сергей, напишите третье число)");
+let res;
+
+
+res = (Number(x) + Number(y) + Number(z)) / 3;
+console.log(res);
+alert(res);
