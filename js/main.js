6be1bfbb46a7ec62a1dc956cdd6d541686b9ae41
Vlad Vobiachenko <109553178+vladosi1@users.noreply.github.com>
2023-02-23 00:17:57 +0200

    sec comm

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let z = prompt("Сергей, напишите третье число)");
 let res;
 
 
+
 res = (Number(x) + Number(y) + Number(z)) / 3;
-console.log(res);
-alert(res);
+
+alert("Среднее арифметическое число: " + res);
